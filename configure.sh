8f7808db3c77cbbcd4dfe59a9e68038bf67c4a48
j-sattler <mail@sattlerjoshua.de>
2021-01-21 18:12:53 +0100

    Intial commit

---
+++ configure.sh
@@ -0,0 +1,7 @@
+git config --global alias.co checkout
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.br branch
+git config --global alias.st status
+git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'
+git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
