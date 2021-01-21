681f9edeb19093aa1bab0469f6511ce4f60738cc
j-sattler <mail@sattlerjoshua.de>
2021-01-21 19:57:32 +0100

    Add log-like alias

--- configure.sh
+++ configure.sh
@@ -4,4 +4,4 @@ git config --global alias.unstage 'reset HEAD --'
 git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'
-git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
+git config --global alias.ll "log --graph --topo-order --date=short --abbrev-commit --decorate --all --boundary --pretty=format:'%Cgreen%ad %Cred%h%Creset -%C(yellow)%d%Creset %s %Cblue[%cn]%Creset %Cblue%G?%Creset'"
